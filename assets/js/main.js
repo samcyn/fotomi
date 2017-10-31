a5dfe4d018d4bc53443ef222fde68790cc347d54
samcyn <samsoniyanda@outlook.com>
2017-10-31 17:20:18 +0100

    added modals and forms

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,39 @@
 var app = {
-    
+    custombox: function(){
+        var $btn = $(document).find('.modal-btn');
+        if(!$btn){
+            return;
+        }
+        $btn.on('click', function(){
+            var effect = 'scale';
+            var _id = $(this).data('id');
+            
+            if(_id === 'sign_up'){
+                effect = 'flip';
+            }
+            // Instantiate new modal
+            var modal = new Custombox.modal({
+                content: {
+                    effect: 'fadein',
+                    target: '#' + _id
+                },
+                // Options
+                loader : {
+                    active: true,
+                    color: '#000',
+                    speed: 1500,
+                }
+            });
+        
+
+            
+            document.addEventListener('custombox:content:open', function() {
+                Custombox.modal.closeAll();
+            });
+            
+            // Open
+            modal.open();
+        });
+        
+    }
 }
