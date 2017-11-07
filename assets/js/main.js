61a87ca6a3117ebca0bf3c268e593ddcaa5fe217
samcyn <samsoniyanda@outlook.com>
2017-11-07 16:23:20 +0100

    tabs fixed

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,58 +1,11 @@
 var app = {
-    custombox: function(){
-        var $btn = $(document).find('.modal-btn');
-        if(!$btn){
-            return;
-        }
-        $btn.on('click', function(){
-            var effect = 'scale';
-            var _id = $(this).data('id');
-            
-            if(_id === 'sign_up'){
-                effect = 'flip';
-            }
-            // Instantiate new modal
-            var modal = new Custombox.modal({
-                content: {
-                    effect: 'fadein',
-                    target: '#' + _id
-                },
-                // Options
-                loader : {
-                    active: true,
-                    color: '#000',
-                    speed: 1500,
-                }
-            });
-        
-
-            
-            document.addEventListener('custombox:content:open', function() {
-                Custombox.modal.closeAll();
-            });
-            
-            // Open
-            modal.open();
-        });
-        
-    },
-    handler: function(arg){
-        // after some time hide loader
-        setTimeout( function() {
-            loader.hide();
-
-            pageWrap.find('.page_container').removeClass('show');
-            
-            $(arg).addClass('show');
-
-        }, 2000 );
-    },
+   
     loaderHandler: function(){
 
         var pageWrap = $('.wrapper'),
-        pages = $('.page_container');
-        triggers = $('a.pageload-link');
-        checker = '#page_1';
+        pages = $('.page_container'),
+        triggers = $('a.pageload-link'),
+        checker = '#landing_page',
         //console.log(window.location.hash);
         loader = new SVGLoader( document.getElementById( 'loader' ), { speedIn : 400, easingIn : mina.easeinout } );
 
