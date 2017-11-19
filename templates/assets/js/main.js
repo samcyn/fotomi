1be4a7cc1326cfdd893790f10c9a9d95eb0323f4
samcyn <samsoniyanda@outlook.com>
2017-11-20 00:57:38 +0100

    mobile right side bar styled and enhanced

--- templates/assets/js/main.js
+++ templates/assets/js/main.js
@@ -47,17 +47,15 @@ var app = {
         init();
     },
     pageLinks: function(){
-        var _this = this,
+        var _this = this;
        
-        _links =  $('a.pageload-link');
-
-        _links.on("click", function(e){
-           
+       // _links =  $('a.pageload-link');
+        $(document).on("click", 'a.pageload-link', function(e){
             e.preventDefault();
-
+            
             var $this = $(this),
             _checker = window.location.hash || '#landing_page';
-           
+            
             console.log($this[0].hash, _checker);
             if($this[0].hash === _checker){
                 return;
@@ -66,6 +64,22 @@ var app = {
             _this.loaderHandler(_checker);
 
         });
+
+        // _links.on("click", function(e){
+           
+        //     e.preventDefault();
+
+        //     var $this = $(this),
+        //     _checker = window.location.hash || '#landing_page';
+           
+        //     console.log($this[0].hash, _checker);
+        //     if($this[0].hash === _checker){
+        //         return;
+        //     }
+        //     _checker = $this[0].hash;
+        //     _this.loaderHandler(_checker);
+
+        // });
     },
     login_form: function(){
         var _this = this;
@@ -110,6 +124,84 @@ var app = {
         $("#file-upload").on('change', function(){
             var _name = $(this)[0].files[0].name;
             $("#file_name").text(_name);
+        });
+
+    },
+    sidebarController: function(){
+        
+        var navbar_initialized = false;
+
+        lbd = {
+            misc:{
+                navbar_menu_visible: 0
+            },
+            initRightMenu: function(){  
+                if(!navbar_initialized){
+                    //clone the navbar
+                    var $navbar = $('nav').find('.navbar-collapse').first().clone(true);
+                    
+                    console.log($navbar);
+                    
+                    
+                    //define an empty list to add items..
+                    var ul_content = '';
+                    
+                    //add the content from the regular header to the right menu
+                    $navbar.children('ul').each(function(){
+                        ul_content +=   $(this).html();  
+                    });
+                    
+                    ul_content = '<aside class="rightSideBar"><ul class="menus">' + ul_content + '</ul></aside>';
+                    
+                    $('body').append(ul_content);
+                
+                    var $toggle = $('.navbar-toggler');
+                    
+                    $toggle.click(function (){    
+                        if(lbd.misc.navbar_menu_visible == 1) {
+                            $('html').removeClass('nav-open'); 
+                            lbd.misc.navbar_menu_visible = 0;
+                            $('#bodyClick').remove();
+                            setTimeout(function(){
+                                $toggle.removeClass('toggled');
+                            }, 400);
+                        
+                        } else {
+                            setTimeout(function(){
+                                $toggle.addClass('toggled');
+                            }, 430);
+                            
+                            var div = '<div id="bodyClick"></div>';
+                            $(div).appendTo("body").click(function() {
+                                $('html').removeClass('nav-open');
+                                lbd.misc.navbar_menu_visible = 0;
+                                $('#bodyClick').remove();
+                                setTimeout(function(){
+                                    $toggle.removeClass('toggled');
+                                }, 400);
+                            });
+                        
+                            $('html').addClass('nav-open');
+                            lbd.misc.navbar_menu_visible = 1;
+                            
+                        }
+                    });
+                    navbar_initialized = true;
+                }
+
+            }
+        }
+
+        // Init navigation toggle for small screens   
+        if($(window).width() <= 991){
+            lbd.initRightMenu();   
+        }
+
+        // activate collapse right menu when the windows is resized 
+        $(window).resize(function(){
+            if($(window).width() <= 991){
+                lbd.initRightMenu();   
+            }
         });
 
     }
